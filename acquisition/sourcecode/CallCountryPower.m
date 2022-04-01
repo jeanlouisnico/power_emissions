e409cb3ea6a9ba3f2555eab663253dd184ced3b9
jlouis <jlouis@oy2112063.univ.yo.oulu.fi>
2022-04-01 09:24:16 +0300

    handles errors primitivaly

--- acquisition/sourcecode/CallCountryPower.m
+++ acquisition/sourcecode/CallCountryPower.m
@@ -7,103 +7,107 @@ function [ENTSOE, TSO, PoweroutLoad, xCHANGE] = CallCountryPower(country)
 ENTSOE       = ENTSOE_exch('country',country,'documentType','Generation') ;
 PoweroutLoad = ENTSOE_exch('country',country,'documentType','Load')       ;
 
-
-switch country
-    %%% France
-    case 'France'
-        [TSO.emissionskit, xCHANGE] = extractFrance ;
-    %%% Sweden
-    % For Sweden, data can be extracted from the TSO website
-    case 'Sweden'
-        TSO = extractSweden ;
-        %%%%% TO GET THROUGH THE www.svk.se API %%%%%%%%
-        xCHANGE = XchangeNordic('SE') ;
-    %%% Russia
-    % For Russia, data can be extracted from the TSO website
-    case 'Russia'
-        TSO = extractRussia ;
-        %%%%% TO GET THROUGH THE br.so-ups.ru API %%%%%%%%
-        xCHANGE = XchangeNordic('RU') ;
-    %%% Estonia
-    % For Estonia, data can be extracted from ENTSOE
-    case 'Estonia'
-        %%%%% TO GET THROUGH THE elering API %%%%%%%%
-        TSO    = extractEstonia ;
-        xCHANGE = XchangeNordic('EE') ;
-    %%% Norway
-    % For Norway, data can be extracted from ENTSOE
-    case 'Norway' 
-        %%%%% TO GET THROUGH THE ENTSOE API %%%%%%%%
-        TSO      = extractstatnett('NO') ;
-        xCHANGE = XchangeNordic('NO') ;
-    %%% Germany
-    % For Germany, data can be extracted from ENTSOE
-    case 'Germany' 
-        %%%%% TO GET THROUGH THE ENTSOE API %%%%%%%%
-        TSO    = 0 ;
-        xCHANGE = 0 ;
-    %%% Finland
-    % For Finland, data can be extracted from ENTSOE
-    case 'Finland' 
-        %%%%% TO GET THROUGH THE ENTSOE API %%%%%%%%
-        TSO    = extractFinland ;
-        xCHANGE = XchangeNordic('FI') ;
-    case 'Bulgaria' 
-        %%%%% TO GET THROUGH THE ENTSOE API %%%%%%%%
-        TSO    = BG ;
-        xCHANGE = BG_XChange ;
-    case 'Denmark' 
-        %%%%% TO GET THROUGH THE ENTSOE API %%%%%%%%
-        TSO    = DK ;
-        xCHANGE = XchangeNordic('DK') ;
-    case 'Spain' 
-        %%%%% TO GET THROUGH THE ENTSOE API %%%%%%%%
-        TSO    = ES ;
-        xCHANGE = ESXChange ;
-    case 'Czech' 
-        %%%%% TO GET THROUGH THE ENTSOE API %%%%%%%%
-        TSO    = 0 ;
-        xCHANGE = 0 ;
-    case 'Belgium' 
-        %%%%% TO GET THROUGH THE ENTSOE API %%%%%%%%
-        TSO    = BE ;
-        xCHANGE = BE_Xchange ;
-    case 'Romania' 
-        %%%%% TO GET THROUGH THE ENTSOE API %%%%%%%%
-        TSO    = RO ;
-        xCHANGE = 0 ;
-    case 'Portugal' 
-        %%%%% TO GET THROUGH THE ENTSOE API %%%%%%%%
-        TSO    = PT ;
-        xCHANGE = PTXChange ;
-    case 'Hungary' 
-        %%%%% TO GET THROUGH THE ENTSOE API %%%%%%%%
-        TSO    = HU ;
-        xCHANGE = HU_exchange ;
-    case 'Latvia'
-        %%%%% TO GET THROUGH THE ENTSOE API %%%%%%%%
-        TSO    = LV('Latvia') ;
-        xCHANGE = XchangeNordic('LV') ;
-    case 'Austria'
-        %%%%% TO GET THROUGH THE ENTSOE API %%%%%%%%
-        TSO    = AT ;
-        xCHANGE = AT_exchange ;
-    case 'Poland'
-        %%%%% TO GET THROUGH THE ENTSOE API %%%%%%%%
-        TSO    = PO ;
-        xCHANGE = PO_exchange ;
-    case 'Lithuania'
-        TSO    = LV('Lithuania') ;
-        xCHANGE = XchangeNordic('LT') ;
-    case 'Greece'
-        TSO    = GR ;
-        xCHANGE = 0 ;
-    case 'GB'
-        TSO    = UK ;
-        xCHANGE = UK_Exchange ;
-    case 'Montenegro'
-        [TSO, xCHANGE] = ME ;
-    otherwise
-        TSO = 0 ;
-        xCHANGE = 0 ;
+try
+    switch country
+        %%% France
+        case 'France'
+            [TSO.emissionskit, xCHANGE] = extractFrance ;
+        %%% Sweden
+        % For Sweden, data can be extracted from the TSO website
+        case 'Sweden'
+            TSO = extractSweden ;
+            %%%%% TO GET THROUGH THE www.svk.se API %%%%%%%%
+            xCHANGE = XchangeNordic('SE') ;
+        %%% Russia
+        % For Russia, data can be extracted from the TSO website
+        case 'Russia'
+            TSO = extractRussia ;
+            %%%%% TO GET THROUGH THE br.so-ups.ru API %%%%%%%%
+            xCHANGE = XchangeNordic('RU') ;
+        %%% Estonia
+        % For Estonia, data can be extracted from ENTSOE
+        case 'Estonia'
+            %%%%% TO GET THROUGH THE elering API %%%%%%%%
+            TSO    = extractEstonia ;
+            xCHANGE = XchangeNordic('EE') ;
+        %%% Norway
+        % For Norway, data can be extracted from ENTSOE
+        case 'Norway' 
+            %%%%% TO GET THROUGH THE ENTSOE API %%%%%%%%
+            TSO      = extractstatnett('NO') ;
+            xCHANGE = XchangeNordic('NO') ;
+        %%% Germany
+        % For Germany, data can be extracted from ENTSOE
+        case 'Germany' 
+            %%%%% TO GET THROUGH THE ENTSOE API %%%%%%%%
+            TSO    = 0 ;
+            xCHANGE = 0 ;
+        %%% Finland
+        % For Finland, data can be extracted from ENTSOE
+        case 'Finland' 
+            %%%%% TO GET THROUGH THE ENTSOE API %%%%%%%%
+            TSO    = extractFinland ;
+            xCHANGE = XchangeNordic('FI') ;
+        case 'Bulgaria' 
+            %%%%% TO GET THROUGH THE ENTSOE API %%%%%%%%
+            TSO    = BG ;
+            xCHANGE = BG_XChange ;
+        case 'Denmark' 
+            %%%%% TO GET THROUGH THE ENTSOE API %%%%%%%%
+            TSO    = DK ;
+            xCHANGE = XchangeNordic('DK') ;
+        case 'Spain' 
+            %%%%% TO GET THROUGH THE ENTSOE API %%%%%%%%
+            TSO    = ES ;
+            xCHANGE = ESXChange ;
+        case 'Czech' 
+            %%%%% TO GET THROUGH THE ENTSOE API %%%%%%%%
+            TSO    = 0 ;
+            xCHANGE = 0 ;
+        case 'Belgium' 
+            %%%%% TO GET THROUGH THE ENTSOE API %%%%%%%%
+            TSO    = BE ;
+            xCHANGE = BE_Xchange ;
+        case 'Romania' 
+            %%%%% TO GET THROUGH THE ENTSOE API %%%%%%%%
+            TSO    = RO ;
+            xCHANGE = 0 ;
+        case 'Portugal' 
+            %%%%% TO GET THROUGH THE ENTSOE API %%%%%%%%
+            TSO    = PT ;
+            xCHANGE = PTXChange ;
+        case 'Hungary' 
+            %%%%% TO GET THROUGH THE ENTSOE API %%%%%%%%
+            TSO    = HU ;
+            xCHANGE = HU_exchange ;
+        case 'Latvia'
+            %%%%% TO GET THROUGH THE ENTSOE API %%%%%%%%
+            TSO    = LV('Latvia') ;
+            xCHANGE = XchangeNordic('LV') ;
+        case 'Austria'
+            %%%%% TO GET THROUGH THE ENTSOE API %%%%%%%%
+            TSO    = AT ;
+            xCHANGE = AT_exchange ;
+        case 'Poland'
+            %%%%% TO GET THROUGH THE ENTSOE API %%%%%%%%
+            TSO    = PO ;
+            xCHANGE = PO_exchange ;
+        case 'Lithuania'
+            TSO    = LV('Lithuania') ;
+            xCHANGE = XchangeNordic('LT') ;
+        case 'Greece'
+            TSO    = GR ;
+            xCHANGE = 0 ;
+        case 'GB'
+            TSO    = UK ;
+            xCHANGE = UK_Exchange ;
+        case 'Montenegro'
+            [TSO, xCHANGE] = ME ;
+        otherwise
+            TSO = 0 ;
+            xCHANGE = 0 ;
+    end
+catch
+    TSO = 0 ;
+    xCHANGE = 0 ;
 end
