6d3e4507dafd175bf30b875d5f5b132bf9d0a89e
jlouis <jlouis@oy2112063.univ.yo.oulu.fi>
2022-04-05 10:24:36 +0300

    added looplog

---
+++ acquisition/sourcecode/looplog.m
@@ -0,0 +1,17 @@
+function looplog(msgin)
+% yourMsg = 'I am alive.'
+
+p = mfilename('fullpath') ;
+[filepath,~,~] = fileparts(p) ;
+fparts = split(filepath, filesep) ;
+fparts = join(fparts(1:end), filesep) ;
+
+% save([fparts{1} filesep 'database' filesep 'data_input.mat'],'data_input');
+
+
+fid = fopen(fullfile(fparts{1}, 'fetcher.log'), 'a');
+if fid == -1
+  error('Cannot open log file.');
+end
+fprintf(fid, '%s: %s\n', datestr(now, 0), msgin);
+fclose(fid);
