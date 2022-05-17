26506d55f0f83806963aff0debf187813851281d
jlouis <jlouis@oy2112063.univ.yo.oulu.fi>
2022-05-17 17:46:42 +0300

    sql query chqnged

--- acquisition/sourcecode/clear_xchange_sql.sql
+++ acquisition/sourcecode/clear_xchange_sql.sql
@@ -1,24 +1,12 @@
-WITH cte AS (
-    SELECT 
-        id,
-        date_time, 
-        source, 
-        fromcountry, 
-        tocountry, 
-        ROW_NUMBER() OVER (
-            PARTITION BY 
-                date_time, 
-                source, 
-                fromcountry, 
-                tocountry
-            ORDER BY 
-                date_time, 
-                source, 
-                fromcountry, 
-                tocountry
-        ) row_num
-     FROM 
-        xchange
-)
-DELETE FROM cte
-WHERE row_num > 1;
+DELETE FROM xchange
+WHERE id IN
+    (SELECT id
+    FROM 
+        (SELECT id,
+         ROW_NUMBER() OVER( PARTITION BY date_time,
+         source,
+         fromcountry, 
+         tocountry
+        ORDER BY  id ) AS row_num
+        FROM xchange ) t
+        WHERE t.row_num > 1 );
