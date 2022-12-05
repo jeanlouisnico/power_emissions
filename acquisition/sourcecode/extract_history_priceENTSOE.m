3d704cbae4f58be37503beb193dc8e55c81d9ce5
jlouis <jlouis@oy2112063.univ.yo.oulu.fi>
2022-12-05 22:45:20 +0200

    modified Russia source

--- acquisition/sourcecode/extract_history_priceENTSOE.m
+++ acquisition/sourcecode/extract_history_priceENTSOE.m
@@ -2,7 +2,8 @@ function elsepost_array = extract_history_priceENTSOE(varargin)
 
 defaultupdateall     = false ;     
 defaultupdatemiss    = true ;
-
+defaultcountry       = 'all' ;
+defaultstartingyear  = 2014 ;
 p = inputParser;
 
 pfile = mfilename('fullpath') ;
@@ -16,6 +17,8 @@ fparts = join(fparts(1:end-1), filesep) ;
 
 addParameter(p,'updateall',defaultupdateall, @islogical);
 addParameter(p,'updatemiss',defaultupdatemiss, @islogical);
+addParameter(p,'country',defaultcountry, @ischar);
+addParameter(p,'startingyear',defaultstartingyear, @isnumeric);
 
 parse(p, varargin{:});
 
@@ -30,34 +33,59 @@ elseif results.updatemiss
     
     lastentry = data.elsepost_array.AT.(allyears{end}).at.pricetime(end) ;
     startingyear = lastentry.Year ;
+elseif results.startingyear >= 2015
+    startingyear = results.startingyear ;
 else
     data = load("elspot_prices.mat") ;
-    elsepost_array = data.elsepost_array ;
+    elsepost_array = data.priceout ;
 end
 
 curtime = datetime('now','TimeZone','UTC') ;
 
-if results.updateall || results.updatemiss
+if results.updateall || results.updatemiss || results.startingyear >= 2015
     for idate = startingyear:curtime.Year
-        datestart = datetime(idate,1,1) ;
+        datestart = datetime(idate,1,1,'TimeZone','UTC') ;
         if idate == curtime.Year
             dateend = datetime(curtime - caldays(1)) ;
         else
-            dateend = datetime(idate,12,31,23,59,59) ;
+            dateend = datetime(idate,12,31,23,59,59,'TimeZone','UTC') ;
         end
-        Country = country2fetch ;
-    
+        switch results.country
+            case 'all'
+                Country = country2fetch ;
+            otherwise
+                Country{1} = results.country ;
+        end
         country_code = countrycode(Country) ;
         
         
-        for icountry = 1:length(Country)
-            elsepost_array.(country_code.alpha2{icountry}).(['x' num2str(datestart.Year)]) = elspotENTSOEhist(country_code(icountry,:),'datestart',datestart,'dateend',dateend) ;
+        for icountry = 1:numel(Country)
+            if isa(country_code.alpha2,'cell')
+                switch country_code.alpha2{icountry}
+                    case 'GB'
+                        elsepost_array.(country_code.alpha2{icountry}).(['x' num2str(datestart.Year)]) = UK_price(datestart, dateend) ;
+                    otherwise
+                        elsepost_array.(country_code.alpha2{icountry}).(['x' num2str(datestart.Year)]) = elspotENTSOEhist(country_code(icountry,:),'datestart',datestart,'dateend',dateend) ;
+                end
+            else
+                x = elspotENTSOEhist(country_code(icountry,:),'datestart',datestart,'dateend',dateend) ; 
+                elsepost_array.(country_code.alpha2).(['x' num2str(datestart.Year)]) = x ;
+            end
         end
     end
-
-    save(strjoin({fparts{1},'input','general','elspot_prices.mat'},filesep),'elsepost_array') ;
+    switch results.country
+        case 'all'
+            save(strjoin({fparts{1},'input','general','elspot_prices.mat'},filesep),'elsepost_array') ;
+        otherwise
+            save(strjoin({fparts{1},'input','general',['elspot_prices_' country_code.alpha2{1} '.mat']},filesep),'elsepost_array') ;
+    end
+    priceout = conca_elspot(elsepost_array) ;
+else
+    priceout = createtototal(elsepost_array) ;
 end
-priceout = conca_elspot(elsepost_array) ;
+
+
+
 allgeo = fieldnames(priceout) ;
 
 for igeo = 1:length(allgeo)
@@ -76,7 +104,14 @@ for igeo = 1:length(allgeo)
 end
 
 % Save the extracted data to a json file
-dlmwrite(strjoin({fparts{1},'input','general','elspot_pricesTT.json'},filesep),jsonencode(data3.total, "PrettyPrint", true),'delimiter','');
+switch results.country
+    case 'all'
+        dlmwrite(strjoin({fparts{1},'input','general','elspot_pricesTT.json'},filesep),jsonencode(data3.total, "PrettyPrint", true),'delimiter','');
+        save(strjoin({fparts{1},'input','general','elspot_pricesTT.mat'},filesep),'priceout') ;
+    otherwise
+        dlmwrite(strjoin({fparts{1},'input','general',['elspot_prices_TT_' country_code.alpha2{1} '.json']},filesep),jsonencode(data3.total, "PrettyPrint", true),'delimiter','');
+        save(strjoin({fparts{1},'input','general',['elspot_prices_TT_' country_code.alpha2{1} '.mat']},filesep),'priceout') ;
+end
 
 
-save(strjoin({fparts{1},'input','general','elspot_pricesTT.mat'},filesep),'priceout') ;
+
