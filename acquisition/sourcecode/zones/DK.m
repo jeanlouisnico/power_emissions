a808408cf2e0cbd5af36b9b9e94ea812bf3db734
jeanlouisnico <sqA-sKd-xH8-GS6>
2023-05-31 22:41:35 +0300

    Updated DK and removed py dependency

--- acquisition/sourcecode/zones/DK.m
+++ acquisition/sourcecode/zones/DK.m
@@ -1,62 +1,38 @@
 
 function TTSync = DK
 
-pe = pyenv;
-if isempty(pe.Version)
-    errorlog("Python not installed - Cannot fetch Denmark TSO data --> solution, install python for your machine and define it in MatLab through the PythonEnvironment function") ;
-    return ;
-end
+alldata = webread('https://api.energidataservice.dk/dataset/ElectricityProdex5MinRealtime?offset=0&sort=Minutes5UTC%20DESC&timezone=dk') ;
+alltime = cellfun(@(x) datetime(x, "TimeZone",'UTC'), {alldata.records.Minutes5UTC}') ;
+area = {alldata.records.PriceArea}' ;
 
-p = mfilename('fullpath') ;
-[filepath,~,~] = fileparts(p) ;
-fparts = split(filepath, filesep) ;
-fparts = join(fparts(1:end-2), filesep) ;
-setup_ini = jsondecode(fileread([fparts{1} filesep 'setup' filesep 'ini.json']));
-
-try
-    pythonpath = setup_ini.python.environment ;
-catch
-    errorlog("Python environment not defined - Cannot fetch Denmark TSO data --> solution, create a python environment with the required packages. Define the path of this envs in the setupfetcher function") ;
-    return ;
-end
-
-if ~strcmp(pythonpath(end),filesep)
-    pythonpath = [pythonpath filesep] ;
-end
-system([pythonpath 'python ' '"' filepath filesep 'DK_request.py"']) ;
-
-setup = jsondecode(fileread('data.json'));
-
-alldata = setup.data.electricityprodex5minrealtime ;
-
-% power.windoff = [setup.data.electricityprodex5minrealtime(:).OffshoreWindPower]';
-% power.windon  = [setup.data.electricityprodex5minrealtime(:).OnshoreWindPower]';
-
-alltime = datetime(cat(1, alldata(:).Minutes5UTC) , 'Format', 'uuuu-MM-dd''T''HH:mm:ss+00:00', 'TimeZone', 'UTC') ;
-
-alltimeDK = datetime(alltime, 'TimeZone', 'Europe/Copenhagen') ;
-
-allfields = fieldnames(alldata) ;
-Area = cat(1, alldata(:).PriceArea) ;
+allfields = fieldnames(alldata.records) ;
 varnames = {} ;
 for ifield = 1:length(allfields)
     switch allfields{ifield}
         case {'Minutes5UTC' 'Minutes5DK' 'PriceArea'}
 
         otherwise
-            datain = {setup.data.electricityprodex5minrealtime(:).(allfields{ifield})}';
+            datain = {alldata.records.(allfields{ifield})}';
             dataempty = cellfun(@(x) isempty(x), datain) ;
-            datain(dataempty) = {NaN} ;
+            datain(dataempty) = {0} ;
             powerout.(makevalidstring(allfields{ifield})) = cell2mat(datain) ;
             varnames = [varnames allfields(ifield)];
     end
 end
 
-power = array2timetable(struct2array(powerout), 'RowTimes', alltime, 'VariableNames',varnames) ;
-power  = addvars(power, Area) ;
-
-TTSync.(power(1,'Area').Variables).TSO = power(1,:) ;
-TTSync.(power(2,'Area').Variables).TSO = power(2,:) ;
+areacode = unique(area) ;
+powerfields = fieldnames(powerout) ;
+for icode = 1:length(areacode)
+    codename = areacode{icode} ;
+    id_extract = strcmp(area, codename) ; 
+    timeextract = alltime(id_extract) ;
+    for ifield = 1:length(powerfields)
+        p.(powerfields{ifield}) = powerout.(powerfields{ifield})(id_extract) ;
+    end
+    rtemp = struct2table(p) ;
+    aa = sortrows(table2timetable(rtemp, "RowTimes",timeextract)) ;
+    TTSync.(codename).TSO = aa(end,:) ;
+end
 
 elecfuel = retrieveEF ;
 
@@ -117,53 +93,38 @@ extractcountry_fuel = {'biomass' 'coal_chp' 'other' 'gas' 'oil_chp' 'waste'} ;
 extracteurostat = {'biomass' 'coal' 'unknown' 'gas' 'oil' 'waste'} ;
 
 init        = normalisedpredictthermal(end,extracteurostat).Variables / 100 ;
-thermalpower.(power(1,'Area').Variables) = TTSync.(power(1,'Area').Variables).TSO.ProductionLt100MW ;
-thermalpower.(power(2,'Area').Variables) = TTSync.(power(2,'Area').Variables).TSO.ProductionLt100MW ;
 
-newlimitL.(power(1,'Area').Variables)    = array2table(eneroutL100(1,extractcountry_fuel).Variables/sum(eneroutL100.Variables) * thermalpower.(power(1,'Area').Variables),'VariableNames', extractcountry_fuel)  ;
-newlimitL.(power(2,'Area').Variables)    = array2table(eneroutL100(1,extractcountry_fuel).Variables/sum(eneroutL100.Variables) * thermalpower.(power(2,'Area').Variables),'VariableNames', extractcountry_fuel)  ;
+for icode = 1:length(areacode)
+    codename = areacode{icode} ;
+    thermalpower.(codename) = TTSync.(codename).TSO.(makevalidstring('ProductionLt100MW'))(end) ;
+    newlimitL.(codename)    = array2table(eneroutL100(1,extractcountry_fuel).Variables/sum(eneroutL100.Variables) * thermalpower.(codename),'VariableNames', extractcountry_fuel)  ;
+    thermalpower.(codename) = TTSync.(codename).TSO.(makevalidstring('ProductionGe100MW'))(end) ;
+    newlimitH.(codename)   = array2table(eneroutG100(1,extractcountry_fuel).Variables/sum(eneroutG100.Variables) * thermalpower.(codename),'VariableNames', extractcountry_fuel)  ;
+    prodfuel.(codename) = array2table(newlimitL.(codename).Variables + newlimitH.(codename).Variables,'VariableNames', extractcountry_fuel)  ;
 
-thermalpower.(power(1,'Area').Variables) = TTSync.(power(1,'Area').Variables).TSO.ProductionGe100MW ;
-thermalpower.(power(2,'Area').Variables) = TTSync.(power(2,'Area').Variables).TSO.ProductionGe100MW ;
+end
 
-newlimitH.(power(1,'Area').Variables)   = array2table(eneroutG100(1,extractcountry_fuel).Variables/sum(eneroutG100.Variables) * thermalpower.(power(1,'Area').Variables),'VariableNames', extractcountry_fuel)  ;
-newlimitH.(power(2,'Area').Variables)   = array2table(eneroutG100(1,extractcountry_fuel).Variables/sum(eneroutG100.Variables) * thermalpower.(power(2,'Area').Variables),'VariableNames', extractcountry_fuel)  ;
+for icode = 1:length(areacode)
+    codename = areacode{icode} ;
+    test = table ;
+        test = addvars(test,TTSync.(codename).TSO.(makevalidstring('OffshoreWindPower'))(end),'NewVariableNames', 'windoff') ;
+        test = addvars(test,TTSync.(codename).TSO.(makevalidstring('OnshoreWindPower'))(end),'NewVariableNames', 'windon') ;
+        test = addvars(test,TTSync.(codename).TSO.(makevalidstring('SolarPower'))(end),'NewVariableNames', 'solar') ;
+        test = addvars(test, prodfuel.(codename).biomass,'NewVariableNames', 'biomass') ;
+        test = addvars(test, prodfuel.(codename).coal_chp,'NewVariableNames', 'coal_chp') ;
+        test = addvars(test, prodfuel.(codename).other,'NewVariableNames', 'other') ;
+        test = addvars(test, prodfuel.(codename).gas,'NewVariableNames', 'gas') ;
+        test = addvars(test, prodfuel.(codename).oil_chp,'NewVariableNames', 'oil_chp') ;
+        test = addvars(test, prodfuel.(codename).waste,'NewVariableNames', 'waste') ;
+    
+    TTSync.(codename).emissionskit = table2timetable(test,"RowTimes",TTSync.(codename).TSO.Time(end))  ;
+end
 
-prodfuel.(power(1,'Area').Variables) = array2table(newlimitL.(power(1,'Area').Variables).Variables + newlimitH.(power(1,'Area').Variables).Variables,'VariableNames', extractcountry_fuel)  ;
-prodfuel.(power(2,'Area').Variables) = array2table(newlimitL.(power(2,'Area').Variables).Variables + newlimitH.(power(2,'Area').Variables).Variables,'VariableNames', extractcountry_fuel)  ;
+test = TTSync.(areacode{1}).emissionskit.Variables + TTSync.(areacode{2}).emissionskit.Variables ;
+TTSync.emissionskit = array2timetable(test, 'VariableNames',TTSync.(areacode{2}).emissionskit.Properties.VariableNames, 'RowTimes',TTSync.(areacode{2}).emissionskit.Time) ;
 
-test = TTSync.(power(1,'Area').Variables).TSO ;
-    test = removevars(test,{'ProductionGe100MW','ProductionLt100MW','OffshoreWindPower', 'OnshoreWindPower', 'SolarPower', 'ExchangeGreatBelt', 'ExchangeGermany', ...
-                            'ExchangeNetherlands', 'ExchangeNorway', 'ExchangeSweden', 'BornholmSE4', 'Area'}) ;
-    test = addvars(test,TTSync.(power(1,'Area').Variables).TSO.OffshoreWindPower,'NewVariableNames', 'windoff') ;
-    test = addvars(test,TTSync.(power(1,'Area').Variables).TSO.OnshoreWindPower,'NewVariableNames', 'windon') ;
-    test = addvars(test,TTSync.(power(1,'Area').Variables).TSO.SolarPower,'NewVariableNames', 'solar') ;
-    test = addvars(test, prodfuel.(power(1,'Area').Variables).biomass,'NewVariableNames', 'biomass') ;
-    test = addvars(test, prodfuel.(power(1,'Area').Variables).coal_chp,'NewVariableNames', 'coal_chp') ;
-    test = addvars(test, prodfuel.(power(1,'Area').Variables).other,'NewVariableNames', 'other') ;
-    test = addvars(test, prodfuel.(power(1,'Area').Variables).gas,'NewVariableNames', 'gas') ;
-    test = addvars(test, prodfuel.(power(1,'Area').Variables).oil_chp,'NewVariableNames', 'oil_chp') ;
-    test = addvars(test, prodfuel.(power(1,'Area').Variables).waste,'NewVariableNames', 'waste') ;
+test = TTSync.(areacode{1}).TSO.Variables + TTSync.(areacode{2}).TSO.Variables ;
+TTSync.TSO = array2timetable(test, 'VariableNames',TTSync.(areacode{2}).TSO.Properties.VariableNames, 'RowTimes',TTSync.(areacode{2}).TSO.Time) ;
 
-TTSync.(power(1,'Area').Variables).emissionskit = test ;
-
-test = TTSync.(power(2,'Area').Variables).TSO ;
-    test = removevars(test,{'ProductionGe100MW','ProductionLt100MW','OffshoreWindPower', 'OnshoreWindPower', 'SolarPower', 'ExchangeGreatBelt', 'ExchangeGermany', ...
-                            'ExchangeNetherlands', 'ExchangeNorway','ExchangeSweden', 'BornholmSE4', 'Area'}) ;
-    test = addvars(test,TTSync.(power(2,'Area').Variables).TSO.OffshoreWindPower,'NewVariableNames', 'windoff') ;
-    test = addvars(test,TTSync.(power(2,'Area').Variables).TSO.OnshoreWindPower,'NewVariableNames', 'windon') ;
-    test = addvars(test,TTSync.(power(2,'Area').Variables).TSO.SolarPower,'NewVariableNames', 'solar') ;
-    test = addvars(test, prodfuel.(power(2,'Area').Variables).biomass,'NewVariableNames', 'biomass') ;
-    test = addvars(test, prodfuel.(power(2,'Area').Variables).coal_chp,'NewVariableNames', 'coal_chp') ;
-    test = addvars(test, prodfuel.(power(2,'Area').Variables).other,'NewVariableNames', 'other') ;
-    test = addvars(test, prodfuel.(power(2,'Area').Variables).gas,'NewVariableNames', 'gas') ;
-    test = addvars(test, prodfuel.(power(2,'Area').Variables).oil_chp,'NewVariableNames', 'oil_chp') ;
-    test = addvars(test, prodfuel.(power(2,'Area').Variables).waste,'NewVariableNames', 'waste') ;
-
-TTSync.(power(2,'Area').Variables).emissionskit = test ;
-
-
-test = TTSync.(power(1,'Area').Variables).emissionskit.Variables + TTSync.(power(2,'Area').Variables).emissionskit.Variables ;
-TTSync.emissionskit = array2timetable(test, 'VariableNames',TTSync.(power(2,'Area').Variables).emissionskit.Properties.VariableNames, 'RowTimes',TTSync.(power(2,'Area').Variables).emissionskit.Time) ;
 
 TTSync = rmfield(TTSync,{'DK1', 'DK2'});
